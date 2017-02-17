e35f5fa93878b83629ba957b512405f2d2fd6331
hritvik taneja <hritviktaneja@HRITVIKs-MacBook-Air.local>
2017-02-17 22:45:35 +0530

    error stuff

---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var fs = require('fs');
+http.createServer(action).listen(8888);
+console.log("server is now running");
+
+function action(request,response){
+	if(request.url=='/'&&request.method=='GET'){
+		response.writeHead(200,{"Content-type":"text/html"});
+		fs.createReadStream("./untitled.html").pipe(response);
+	}else if(request.url=='/myaction?timepass=himy&butt=submit'&&request.method=='get'){
+		response.writeHead(200,{"Content-type":"text/html"});
+		fs.createReadStream("./patani.html").pipe(response);
+	}
+}
+
